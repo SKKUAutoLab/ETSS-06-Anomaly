2ccee31625d4e47b2994494b0590c2050491d93a
TDC <tdc2000@g.skku.edu>
2026-02-23 18:57:10 +0900

    Üpdate script

--- scripts/clean_ubuntu.sh
+++ scripts/clean_ubuntu.sh
@@ -6,3 +6,28 @@ find . -name gitignore -type d -exec rm -rf {} \;
 rm -rf /tmp/*
 sudo apt-get autoremove -y
 sudo apt-get autoclean -y
+sudo ap-get clean -y
+sudo apt-get autopurge -y
+dpkg -l | grep '^rc' | awk '{print $2}' | xargs -r sudo apt purge -y
+rm -rf ~/.cache/thumbnails/*
+rm -rf ~/.cache/*
+sudo apt-get autoremove --purge
+sudo rm -rf /var/crash/*
+sudo rm -rf /var/lib/snapd/cache/*
+sudo apt purge $(dpkg -l | awk '/^rc/ {print $2}')
+sudo rm -rf /var/cache/apt/archives/*
+rm -rf ~/.local/share/Trash/*
+sudo find /var/log -type f -name "*.log" -mtime +30 -delete
+sudo find /var/log -type f -name "*.gz" -mtime +60 -delete
+sudo find /tmp -type f -atime +7 -delete
+sudo snap list --all | awk '/disabled/{print $1, $3}' | while read snapname revision; do sudo snap remove "$snapname" --revision="$revision"; done
+sudo rm -rf /var/cache/snapd/*
+sudo journalctl --vacuum-time=7d
+sudo sync && echo 1 | sudo tee /proc/sys/vm/drop_caches
+sudo sync && echo 2 | sudo tee /proc/sys/vm/drop_caches
+sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+sudo swapoff -a && sudo swapon -a
+# sudo journalctl --vacuum-size=100M
+# dpkg --list | grep linux-image
+# sudo apt purge linux-image-5.15.0-73-generic
+# sudo apt autoremove --purge $(dpkg -l 'linux-image-*' | grep '^ii' | awk '{print $2}' | grep -v $(uname -r))
