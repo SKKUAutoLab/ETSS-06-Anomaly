9a7e0cac06585db8dd2291f0bc19aae6fae40802
TDC <tdc2000@g.skku.edu>
2026-03-18 15:43:01 +0900

    Update scripts

---
+++ scripts/install_kiro.sh
@@ -0,0 +1,6 @@
+# https://kiro.dev/docs/cli/reference/cli-commands/
+curl -fsSL https://cli.kiro.dev/install | bash
+kiro-cli update
+kiro-cli login
+# kiro-cli logout
+# kiro-cli version
---
+++ scripts/uninstall_kiro.sh
@@ -0,0 +1,11 @@
+kiro-cli uninstall
+sudo apt-get remove kiro-cli
+sudo apt-get remove kiro-cli*
+sudo apt-get remove kiro*
+sudo apt-get purge kiro-cli
+sudo apt-get --purge autoremove kiro-cli
+sudo apt-get purge --auto-remove kiro-cli
+sudo dpkg --purge --force-depends kiro-cli
+sudo dpkg --purge --force-all kiro-cli
+sudo apt-get autoremove
+sudo apt-get autoclean
