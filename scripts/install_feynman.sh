a60e0a8a3d64d0df0e0a23ca6a8615436f65b375
TDC <tdc2000@g.skku.edu>
2026-04-01 14:49:15 +0900

    Update scripts

---
+++ scripts/install_feynman.sh
@@ -0,0 +1,1 @@
+curl -fsSL https://feynman.is/install | bash
---
+++ scripts/uninstall_feynman.sh
@@ -0,0 +1,19 @@
+sudo rm -f ~/.local/bin/feynman
+sudo rm -rf ~/.local/share/feynman
+sudo rm -rf ~/.config/feynman
+sudo sudo rm -rf /opt/feynman
+sudo rm -rf /usr/local/lib/feynman
+sudo rm -f /usr/local/bin/feynman
+sudo rm -rf ~/.feynman
+sudo rm -rf ~/.codex/skills/feynman
+feynman --version
+which feynman
+sudo apt-get remove feynman
+sudo apt-get remove feynman*
+sudo apt-get purge feynman
+sudo apt-get --purge autoremove feynman
+sudo apt-get purge --auto-remove feynman
+sudo dpkg --purge --force-depends feynman
+sudo dpkg --purge --force-all feynman
+sudo apt-get autoremove
+sudo apt-get autoclean
