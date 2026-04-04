2ade3f758791e4a659571c540988b3090b3673de
TDC <tdc2000@g.skku.edu>
2026-04-04 13:39:22 +0900

    Update script

---
+++ scripts/run_claude.sh
@@ -0,0 +1,6 @@
+export ANTHROPIC_BASE_URL=https://openrouter.ai/api
+export ANTHROPIC_AUTH_TOKEN=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
+export ANTHROPIC_DEFAULT_OPUS_MODEL="qwen/qwen3.6-plus:free"
+export ANTHROPIC_DEFAULT_SONNET_MODEL="qwen/qwen3.6-plus:free"
+export ANTHROPIC_DEFAULT_HAIKU_MODEL="qwen/qwen3.6-plus:free"
+claude
